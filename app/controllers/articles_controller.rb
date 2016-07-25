be086f26c3cd51594b8ae60e6dce2aa216700d3a
Marko <markogardner@gmail.com>
2016-07-25 15:46:42 -0700

    Fix create, add flash messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,16 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article].inspect #This will display what was sent when created
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'  #Renders the new page agein /articles/new
+		end
+	end
+
+	def show
+	  @article = Article.find(params[:id])		
 	end
 
 	private
