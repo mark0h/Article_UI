fc0c0c842d0289c33226bb384515b8912b6e61d3
Marko <markogardner@gmail.com>
2016-07-27 15:41:07 -0700

    added user name length validation, added email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User <ActiveRecord::Base
-
+	validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+	VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
+	validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 6, maximum: 80}, format: {with: VALID_EMAIL_REGEX}
 end
