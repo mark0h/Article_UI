5ad6e63172e107b9be28c0d422cca5e10a4f0dc8
Marko <markogardner@gmail.com>
2016-07-28 10:16:17 -0700

    added admin functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
 	  end
 
 	  def require_same_user
-	  	if current_user != @article.user
+	  	if current_user != @article.user and !current_user.admin?
 	  		flash[:danger] = "HEY! Stop trying to edit/delete other user's articles!!"
 	  		redirect_to root_path
 	  	end
