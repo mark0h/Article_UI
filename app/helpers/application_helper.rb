28b34cb7e46ef9f8394941e53361e60ee9dc728f
Marko <markogardner@gmail.com>
2016-07-27 18:02:14 -0700

    Created partial for article show, for all and individual user, added gravatar method to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	def gravatar_for(user, options = {size: 80})
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase) #gets the MD5 value of the user's email for gravatar.com image
+		size = options[:size]
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: user.username, class: "img-circle")
+	end
 end
